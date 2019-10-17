1aad9f972f857fb9ef585a78f4c88a4d25ac91a6
Konsta Kontunen <konsta.kontunen@ubisoft.com>
2019-10-17 18:01:43 +0300

    Debugging Engine Code

--- UnrealProject/Source/PuzzlePlatforms/Private/PuzzlePlatforms_GI.cpp
+++ UnrealProject/Source/PuzzlePlatforms/Private/PuzzlePlatforms_GI.cpp
@@ -13,7 +13,7 @@
 #include "OnlineSessionSettings.h"
 #include "Engine/Engine.h"
 
-const static FName SESSION_NAME = TEXT("My Session Game");
+const static FName SESSION_NAME = NAME_GameSession;
 const static FName SERVER_NAME_SETTINGS_KEY = TEXT("ServerName");
 
 UPuzzlePlatforms_GI::UPuzzlePlatforms_GI(const FObjectInitializer& ObjectInitializer)
